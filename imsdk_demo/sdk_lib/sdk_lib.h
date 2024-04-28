9973559a943f67bd200cfbf4dd650a3149bf8063
liujingfeng <13121693060@163.com>
2024-04-28 17:39:00 +0800

    feat: framework工程并入demo工程

---
+++ imsdk_demo/sdk_lib/sdk_lib.h
@@ -0,0 +1,18 @@
+//
+//  sdk_lib.h
+//  sdk_lib
+//
+//  Created by 刘警锋 on 2024/4/28.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for sdk_lib.
+FOUNDATION_EXPORT double sdk_libVersionNumber;
+
+//! Project version string for sdk_lib.
+FOUNDATION_EXPORT const unsigned char sdk_libVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sdk_lib/PublicHeader.h>
+
+
